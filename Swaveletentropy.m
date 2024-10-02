c5424bcaaae5af75e0a8acd5aaa58f2cb7077428
xiongtaiyang <1244683773@qq.com>
2024-10-02 15:10:14 +0800

    Update Swaveletentropy.m

--- Swaveletentropy.m
+++ Swaveletentropy.m
@@ -1,17 +1,26 @@
-function SWT = Swaveletentropy(signal,Fs)
-% 输入信号和采样率
+function SWT = Swaveletentropy(signal, Fs)
+% Swaveletentropy - Computes the wavelet entropy of a signal using the 
+%                   Synchrosqueezed Wavelet Transform (SWT).
+%
+% Inputs:
+%   signal - The input signal (1D vector)
+%   Fs - The sampling frequency (scalar)
+%
+% Output:
+%   SWT - The computed wavelet entropy (scalar)
 
-% 执行同步挤压小波变换
-[wt, f] = wsst(signal, Fs);
+% Perform synchrosqueezed wavelet transform (SWT)
+[wt, f] = wsst(signal, Fs);  % wt is the wavelet coefficients, f is the frequency vector
 
-% 计算能量分布
-power = abs(wt).^2;
-total_power = sum(power(:));
-p = power / total_power;
+% Calculate power distribution (energy)
+power = abs(wt).^2;  % Power spectrum (squared magnitude of wavelet coefficients)
+total_power = sum(power(:));  % Total power in the signal
+p = power / total_power;  % Normalize power to get probabilities
 
-% 计算小波熵
-p(p == 0) = eps;   % 避免 log(0)
-SWT = -sum(p(:) .* log(p(:)));
+% Calculate wavelet entropy
+p(p == 0) = eps;   % Replace zero probabilities with a small value (to avoid log(0))
+SWT = -sum(p(:) .* log(p(:)));  % Compute the wavelet entropy
 
-% 显示结果
+% Display the result
 disp(['Wavelet Entropy: ', num2str(SWT)]);
+end
